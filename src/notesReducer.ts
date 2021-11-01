2212e2e202bd9c979c972ce2c8942e2db201b147
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-11-01 19:19:47 +0100

    action redux add note

--- src/notesReducer.ts
+++ src/notesReducer.ts
@@ -1,16 +1,17 @@
-interface NotesState{
+import { Action } from "./actions";
+
+export interface NotesState{
     notes:string[]
 }
 const initialState={
     notes:[]
 }
-type Action ={type:"ADD_NOTE", payload:string}
 export const notesReducer =(state:NotesState=initialState,action:Action)=>{
     switch(action.type){
-    case "ADD_NOTE":{
-        return {...state,notes:[...state.notes,action.payload]}
-    }
-    default:
-        return state;
+        case "ADD_NOTE":{
+            return {...state,notes:[...state.notes,action.payload]}
+        }
+        default:
+            return state;
     }
 }
